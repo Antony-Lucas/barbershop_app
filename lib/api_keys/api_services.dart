02dac9ba3647419602ca816129cfd59b70c1f2a4
Antony Lucas <lawokahama@gmail.com>
2022-05-15 22:10:55 -0300

    'v0.0.1'

--- lib/api_keys/api_services.dart
+++ lib/api_keys/api_services.dart
@@ -1,8 +1,20 @@
 import 'dart:developer';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:barbershop_app/api_keys/api_constants.dart';
 import 'package:barbershop_app/api_keys/user_model.dart';
 
-class ApiService{
-  
+class ApiService {
+  // ignore: body_might_complete_normally_nullable
+  Future<List<Welcome>?> getUsers() async {
+    try {
+      var url = Uri.parse(ApiConstants.urlBase + ApiConstants.usersEndpoint);
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        List<Welcome> _model = welcomeFromJson(response.body);
+        return _model;
+      }
+    } catch (event) {
+      log(event.toString());
+    }
+  }
 }
